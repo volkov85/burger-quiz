60a08b7ab37ebf1396070a43ab9f5a6bf78bd8e9
Volkov85 <xplz2007@gmail.com>
2020-05-16 19:21:57 +0600

    Реализация кнопки бургер-меню

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     const btnOpenModal = document.querySelector('#btnOpenModal');
@@ -6,16 +6,55 @@ document.addEventListener('DOMContentLoaded', function () {
     const closeModal = document.querySelector('#closeModal');
     const questionTitle = document.querySelector('#question');
     const formAnswers = document.querySelector('#formAnswers');
+    const burgerBtn = document.getElementById('burger');
     const answersTitle = 'Стандарт';
     const answersUrl = './image/burger.png';
+
+    let clientWidth = document.documentElement.clientWidth;
+
+    if (clientWidth < 768) {
+        burgerBtn.style.display = 'flex';
+    } else {
+        burgerBtn.style.display = 'none';
+    }
+
+    //Скрытие значка бургер меню при изменении разрешения окна
+    window.addEventListener('resize', () => {
+        clientWidth = document.documentElement.clientWidth;
+
+        if (clientWidth < 768) {
+            burgerBtn.style.display = 'flex';
+        } else {
+            burgerBtn.style.display = 'none';
+        }
+    });
+
+    burgerBtn.addEventListener('click', () => {
+        burgerBtn.classList.add('active');
+        modalBlock.classList.add('d-block');
+        playTest();
+    });
 
     btnOpenModal.addEventListener('click', () => {
         modalBlock.classList.add('d-block');
         playTest();
-    })
+    });
 
     closeModal.addEventListener('click', () => {
         modalBlock.classList.remove('d-block');
+        burgerBtn.classList.remove('active');
+    });
+
+    //Закрытие модального окна при клике на область вне этого окна
+    document.addEventListener('click', (event) => {
+        if (
+            !event.target.closest('.modal-dialog') &&
+            !event.target.closest('.openModalButton') &&
+            !event.target.closest('.burger')
+        )   {
+            modalBlock.classList.remove('d-block');
+            burgerBtn.classList.remove('active');
+        }
     })
 
     const playTest = () => {
